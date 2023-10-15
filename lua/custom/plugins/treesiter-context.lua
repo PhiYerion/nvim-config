9ea4fc7df460b810495668a67ab8f67bdc6b3c1a
PhiYerion <reidbandy@gmail.com>
2023-10-15 03:00:27 -0400

    treesitter-context added

---
+++ lua/custom/plugins/treesiter-context.lua
@@ -0,0 +1,13 @@
+return {
+  'nvim-treesitter/nvim-treesitter-context',
+  enable = true,
+  max_lines = 0,
+  min_window_height = 0,
+  line_numbers = true,
+  multiple_threshold = 5,
+  trim_scope = 'outer',
+  mode = 'cursor',
+  separator = nil,
+  zindex = 20,
+  on_attach = nil,
+}
